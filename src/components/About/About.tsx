cbe77e5dcfccb1828c03f3203a033be59658b4ab
vladislavzaver91 <vladislavzaver91@gmail.com>
2025-01-07 18:20:49 +0200

    feat: added contact info, projects info, about info

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,12 @@
-import { motion } from 'framer-motion'
-import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { ArrowDownFromLine, ArrowUpFromLine } from 'lucide-react'
+import { useMobileModal } from '../../helpers/hooks/useMobileModal'
 import { SkillCard } from '../SkillCard'
 import { SKILL_ITEMS } from './skill-items.data'
 
 const About: React.FC = () => {
+	const { isMenuOpen, toggleMenu } = useMobileModal()
+
 	return (
 		<section className='py-14'>
 			<div className='container mx-auto px-6'>
@@ -29,25 +32,88 @@ const About: React.FC = () => {
 						))}
 					</div>
 
-					<motion.div
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						transition={{ delay: 0.4 }}
-						className='mt-16 text-center'
-					>
+					<div className='mt-16 text-center'>
 						<h2 className='text-2xl font-bold mb-4 text-gray-100'>About Me</h2>
-						<p className='text-red-100 leading-relaxed'>
-							With over 5 years of experience in software testing and
-							automation, I specialize in building scalable test frameworks and
-							implementing efficient CI/CD pipelines. My expertise includes web
-							and API testing, performance testing, and mobile application
-							testing. I'm passionate about staying current with the latest
-							testing methodologies and automation tools to ensure the highest
-							quality of software delivery.
+						<p className='text-red-100 text-sm leading-relaxed'>
+							Hi, I'm an experienced Automation QA Engineer with a strong
+							passion for delivering high-quality software solutions.
 						</p>
-					</motion.div>
+						<button
+							onClick={toggleMenu}
+							className='mt-5 m-auto w-10 h-10 text-white rounded-full flex items-center justify-center transition group'
+							aria-label='Expand About Me'
+						>
+							<ArrowUpFromLine className='w-6 h-6 group-hover:text-blue-400/50 group-hover:shadow-blue-500/10 transition-all' />
+						</button>
+					</div>
 				</motion.div>
 			</div>
+
+			{/* Modal */}
+			<AnimatePresence>
+				{isMenuOpen && (
+					<motion.div
+						className='menu-modal fixed inset-0 bg-black bg-opacity-50 z-50 flex items-end justify-center'
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 0.3 }}
+					>
+						<motion.div
+							initial={{ y: '100%' }}
+							animate={{ y: 0 }}
+							exit={{ y: '100%' }}
+							transition={{ duration: 0.5, ease: 'easeInOut' }}
+							className='bg-gray-800 text-white w-full max-w-4xl p-6 rounded-t-lg shadow-lg overflow-y-auto  max-md:h-full'
+						>
+							<div className='relative'>
+								<h2 className='text-2xl font-bold mb-4 text-gray-100 text-center mt-5'>
+									About Me
+								</h2>
+								<p className='text-sm leading-relaxed'>
+									Hi, I'm an experienced Automation QA Engineer with a strong
+									passion for delivering high-quality software solutions. Over
+									the years, I've honed my skills in building robust testing
+									frameworks and automating complex workflows using tools like
+									JavaScript, TypeScript, Java, Selenium, Playwright, and more.
+									My expertise spans across industries such as AdTech,
+									Healthcare, Finance, and Gambling, giving me a versatile
+									perspective on quality assurance and software testing.
+									<br />
+									Highlights of My Work:
+									<br />
+									Framework Development: Built custom testing frameworks from
+									scratch to streamline automation processes and improve test
+									coverage. <br />
+									Team Leadership: Guided QA teams, mentored members, and
+									conducted code reviews to foster continuous improvement.
+									<br />
+									CI/CD Expertise: Set up and maintained Jenkins pipelines with
+									Docker, ensuring smooth integration and automated test
+									execution. <br />
+									Cross-Platform Testing: Performed rigorous API, UI, and mobile
+									testing for Android, iOS, and web applications using modern
+									tools like Appium and Perfecto. <br />
+									Customer Collaboration: Bridged the gap between development
+									and client teams to align expectations and deliver exceptional
+									results.
+									<br />
+									If you're looking for someone who can build innovative
+									solutions, lead teams effectively, and contribute to
+									delivering exceptional products, let's connect!
+								</p>
+								<button
+									onClick={toggleMenu}
+									className='absolute -top-10 right-1/2 w-8 h-8 text-white rounded-full flex items-center justify-center transition group'
+									aria-label='Close Modal'
+								>
+									<ArrowDownFromLine className='w-6 h-6 group-hover:text-blue-400/50 group-hover:shadow-blue-500/10 transition-all' />
+								</button>
+							</div>
+						</motion.div>
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</section>
 	)
 }
--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,152 +1,166 @@
-import { Suspense, useRef } from "react";
-import { motion } from "framer-motion";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Navigation, Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Center, OrbitControls, useGLTF } from "@react-three/drei";
-import "./loader.css";
+import { Center, OrbitControls, useGLTF } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { motion } from 'framer-motion'
+import { Suspense, useRef } from 'react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import { Navigation, Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import './loader.css'
 function Model({ url, scale, rotation }) {
-  const { scene } = useGLTF(url); // Загружаем GLTF модель
-  const meshRef = useRef();
+	const { scene } = useGLTF(url) // Загружаем GLTF модель
+	const meshRef = useRef()
 
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y += 0.01; // Вращение вокруг оси Y
-    }
-  });
+	useFrame(() => {
+		if (meshRef.current) {
+			meshRef.current.rotation.y += 0.01 // Вращение вокруг оси Y
+		}
+	})
 
-  return (
-    <Center>
-      <primitive
-        object={scene}
-        scale={scale} // Масштабирование модели
-        rotation={rotation} // Применяем вращение
-        ref={meshRef}
-      />
-    </Center>
-  );
+	return (
+		<Center>
+			<primitive
+				object={scene}
+				scale={scale} // Масштабирование модели
+				rotation={rotation} // Применяем вращение
+				ref={meshRef}
+			/>
+		</Center>
+	)
 }
 
 function Scene({ url, scale, rotation }) {
-  return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Suspense fallback={null}>
-        <Model url={url} scale={scale} rotation={rotation} />
-        <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
-      </Suspense>
-    </Canvas>
-  );
+	return (
+		<Canvas>
+			<ambientLight intensity={0.5} />
+			<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+			<pointLight position={[-10, -10, -10]} />
+			<Suspense fallback={null}>
+				<Model url={url} scale={scale} rotation={rotation} />
+				<OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
+			</Suspense>
+		</Canvas>
+	)
 }
 
 const Experience = () => {
-  return (
-    <section className="py-14">
-      <div className="container mx-auto px-6 relative">
-        <Swiper
-          direction="vertical"
-          spaceBetween={50}
-          slidesPerView={1}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Navigation, Pagination]}
-          className="mb-20"
-        >
-          {PROJECT_ITEMS.map((project, index) => {
-            // Разворачиваем первую модель лицом
-            const rotation = index === 0 ? [90, Math.PI / 6, 660] : [0, 0, 0]; // Для первой модели, вращаем на 180 градусов по оси Y
+	return (
+		<section className='py-14'>
+			<div className='container mx-auto px-6 relative'>
+				<Swiper
+					direction='vertical'
+					spaceBetween={50}
+					slidesPerView={1}
+					pagination={{
+						clickable: true,
+					}}
+					modules={[Navigation, Pagination]}
+					className='mb-20'
+				>
+					{PROJECT_ITEMS.map((project, index) => {
+						// Разворачиваем первую модель лицом
+						const rotation = index === 0 ? [90, Math.PI / 6, 660] : [0, 0, 0] // Для первой модели, вращаем на 180 градусов по оси Y
 
-            return (
-              <SwiperSlide key={project.id}>
-                <motion.div
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  className="grid md:grid-cols-2 gap-12 items-center h-full cursor-grab"
-                >
-                  <div>
-                    <motion.h2
-                      key={`title-${project.id}`}
-                      initial={{ opacity: 0, x: 20 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      className="relative text-3xl md:text-4xl font-bold mb-6 bg-gradient-to-r from-gray-100 to-gray-500 text-transparent bg-clip-text"
-                    >
-                      <div className="absolute left-0 top-24 transform -translate-y-1/2 bg-blue-800 w-14 h-48 -z-10" />
-                      {project.title}
-                    </motion.h2>
-                    <motion.p
-                      key={`desc-${project.id}`}
-                      initial={{ opacity: 0, x: 20 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      transition={{ delay: 0.1 }}
-                      className="text-gray-50 text-xl mb-8"
-                    >
-                      {project.description}
-                    </motion.p>
-                    <div className="flex flex-wrap gap-3 mb-8">
-                      {project.technologies.map((tech) => (
-                        <span
-                          key={tech}
-                          className="px-4 py-2 text-lg border border-gray-600 text-gray-100 backdrop-blur-md rounded-lg"
-                        >
-                          {tech}
-                        </span>
-                      ))}
-                    </div>
-                  </div>
-                  <motion.div
-                    key={project.id}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.5 }}
-                    className="h-[600px]"
-                  >
-                    <Scene
-                      url={project.modelUrl}
-                      scale={index === 1 || index === 2 ? [7, 7, 7] : [0.9, 0.9, 0.9]} // Увеличиваем вторую и третью модели в 7 раз, первую немного уменьшаем
-                      rotation={rotation} // Применяем вращение для первой модели
-                    />
-                  </motion.div>
-                </motion.div>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-      </div>
-    </section>
-  );
-};
+						return (
+							<SwiperSlide key={project.id}>
+								<motion.div
+									initial={{ opacity: 0, y: 20 }}
+									animate={{ opacity: 1, y: 0 }}
+									className='grid md:grid-cols-2 gap-12 items-center h-full cursor-grab'
+								>
+									<div>
+										<motion.h2
+											key={`title-${project.id}`}
+											initial={{ opacity: 0, x: 20 }}
+											animate={{ opacity: 1, x: 0 }}
+											className='relative text-2xl md:text-3xl font-bold mb-6 bg-gradient-to-r from-gray-100 to-gray-500 text-transparent bg-clip-text'
+										>
+											<div className='absolute left-0 top-24 transform -translate-y-1/2 bg-blue-800 w-14 h-48 -z-10' />
+											{project.title}
+										</motion.h2>
+										<motion.p
+											key={`desc-${project.id}`}
+											initial={{ opacity: 0, x: 20 }}
+											animate={{ opacity: 1, x: 0 }}
+											transition={{ delay: 0.1 }}
+											className='text-gray-50 text-lg mb-8 whitespace-pre-line'
+										>
+											{project.description
+												.split('•')
+												.filter(Boolean) // Убираем пустые строки, если они есть
+												.map((sentence, idx) => (
+													<span key={idx} className='block'>
+														• {sentence.trim()}
+													</span>
+												))}
+										</motion.p>
+										<div className='flex flex-wrap gap-3 mb-8'>
+											{project.technologies.map(tech => (
+												<span
+													key={tech}
+													className='px-4 py-2 text-lg border border-gray-600 text-gray-100 backdrop-blur-md rounded-lg'
+												>
+													{tech}
+												</span>
+											))}
+										</div>
+									</div>
+									<motion.div
+										key={project.id}
+										initial={{ opacity: 0 }}
+										animate={{ opacity: 1 }}
+										transition={{ duration: 0.5 }}
+										className='h-[600px]'
+									>
+										<Scene
+											url={project.modelUrl}
+											scale={
+												index === 1 || index === 2 ? [7, 7, 7] : [0.9, 0.9, 0.9]
+											} // Увеличиваем вторую и третью модели в 7 раз, первую немного уменьшаем
+											rotation={rotation} // Применяем вращение для первой модели
+										/>
+									</motion.div>
+								</motion.div>
+							</SwiperSlide>
+						)
+					})}
+				</Swiper>
+			</div>
+		</section>
+	)
+}
 
-export default Experience;
+export default Experience
 
 export const PROJECT_ITEMS = [
-  {
-    id: 1,
-    title: "E-commerce Testing Framework",
-    description:
-      "Developed and maintained an end-to-end testing framework for a large e-commerce platform.",
-    technologies: ["Cypress", "TypeScript", "GitHub Actions"],
-    modelUrl: "/mechanical_keyboard_-_aesthetic.glb",
-  },
-  {
-    id: 2,
-    title: "API Testing Suite",
-    description:
-      "Created comprehensive API testing suite with automated performance testing.",
-    technologies: ["Postman", "Newman", "Jenkins", "JMeter"],
-    modelUrl: "/28e9373b8550488bac26b3c35ac2225a.glb",
-  },
-  {
-    id: 3,
-    title: "Mobile App Testing",
-    description:
-      "Implemented automated testing for iOS and Android applications.",
-    technologies: ["Appium", "Java", "TestNG", "BrowserStack"],
-    modelUrl: "/asus_rog_zephyrus_g14_2024.glb",
-  },
-];
+	{
+		id: 1,
+		title: 'Automation JS/TS QA Engineer, Clinch - AdTech',
+		description: `• Built a testing framework from scratch using JavaScript, TypeScript, and Playwright 
+        • Set up Jenkins pipelines with Docker for automated test execution 
+        • Integrated the framework with external services and tools to enhance functionality 
+        • Adding features and optimizing the framework for continuous improvement 
+        • Writing and executing regression, API, and UI tests suites 
+        • Leading the QA team, overseeing process optimization and continuous improvement 
+        • Mentoring QA team members, conducting code reviews, and setting team goals`,
+		technologies: ['Cypress', 'TypeScript', 'GitHub Actions'],
+		modelUrl: '/mechanical_keyboard_-_aesthetic.glb',
+	},
+	{
+		id: 2,
+		title: 'Automation JavaScript QA Engineer, GlobalLogic - Healthcare',
+		description:
+			'• Developing automated test cases using JavaScript and Selenium Web-Driver • Requirements, documentation analysis. Regression investigation via Jenkins CI/CD • Test planning. Customer communication. Test design covering, risks analysis • Executing automated scripts using Appium, Perfecto for both Android and iOS • Manual testing using real devices, iOS, Android, insulin pump emulator • Utilized various testing methods such as White-Box, Black-Box, Exploratory etc',
+		technologies: ['Postman', 'Newman', 'Jenkins', 'JMeter'],
+		modelUrl: '/28e9373b8550488bac26b3c35ac2225a.glb',
+	},
+	{
+		id: 3,
+		title: 'Mobile App Testing',
+		description:
+			'Implemented automated testing for iOS and Android applications.',
+		technologies: ['Appium', 'Java', 'TestNG', 'BrowserStack'],
+		modelUrl: '/asus_rog_zephyrus_g14_2024.glb',
+	},
+]
